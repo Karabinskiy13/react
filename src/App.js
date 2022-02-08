340dd36a94db435b0b6867c4ef6f400e72de2b56
Karabinskiy13 <ivan.karabinskiy1308@gmail.com>
2022-02-08 20:50:42 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useReducer, useState} from "react";
+import {Cats} from "./components/Сats"
+import {Dogs} from "./components/Dogs";
+
+const reducer = (state, action) => {
+  switch (action.type){
+    case 'cat':
+      return{...state, cats:[...state.cats, action.payload]}
+    case 'dog':
+      return{...state,dogs:[...state.dogs, action.payload]}
+  }
+}
 
 function App() {
+  const[state, dispatch]=useReducer(reducer, {cats:[], dogs:[] })
+  const[catName, setCatName]=useState('')
+  const [dogName, setDogName]=useState('')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="inputs">
+        <div className="cats-input">
+          <label>Cats :<input onChange={event => setCatName(event.target.value)} type="text"/> </label>
+          <button onClick={()=>dispatch({type: 'cat', payload: catName})}>Send Cat</button>
+        </div>
+        <div className="dogs-input">
+          <label>Dogs :<input onChange={event => setDogName(event.target.value)} type="text"/></label>
+          <button onClick={()=> dispatch({type: 'dog', payload: dogName})}>Send Dog</button>
+        </div>
+        <div className="pets">
+          <div className="cats">
+            <h2>Cats List:</h2>
+            {state.cats.map(cat=>< Cats cat={cat}/>)}
+          </div>
+          <div className="dogs">
+            <h2>Dogs List:</h2>
+            {state.dogs.map(dog=>< Dogs dog={dog}/> )}
+          </div>
+
+        </div>
+      </div>
+
     </div>
   );
 }
---
+++ src/components/Dogs.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Dogs = ({dog}) => {
+    return (
+        <div>
+            {dog}
+        </div>
+    );
+};
+
